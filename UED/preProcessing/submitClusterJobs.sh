4a1b2b7813cc788e44903b6654764baaa9858b4c
khegazy <kareemhegz@gmail.com>
2021-02-18 17:23:22 -0800

    Updated.

--- UED/preProcessing/submitClusterJobs.sh
+++ UED/preProcessing/submitClusterJobs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
-MAINDIR=/reg/neh/home/khegazy/analysis/nitroBenzene/UED
-ROOTOUTDIR=/reg/ued/ana/scratch/nitroBenzene/rootFiles/
+MAINDIR=/cds/home/k/khegazy/analysis/2018/nitrobenzene/UED
+OUTDIR=/cds/group/ued/scratch/khegazy/nitrobenzene/preProcessing/
 FILETORUN=preProcessing.exe
+
+COMPUTECENTERS=false
+
 
 if [ -z "$1" ]; then
   echo "ERROR SUBMITTING JOBS!!!   Must give the type and and optionally the name of the run (i.e Run/Background and 20180701_0746)!"
@@ -23,9 +26,9 @@ else
   RESUBMIT=true
 fi
 
-make clean; make
-sleep 10
-OUTPUTDIR=${ROOTOUTDIR}/logs/
+#make clean; make
+sleep 5
+OUTPUTDIR=${OUTDIR}/logs/
 for file in runLists/runList_${RUNTYPE}-${RUNNAME}
 do
 
@@ -33,13 +36,28 @@ do
 
   CUTBEGINNING=${file#*runList_}
   OUTPUTFILENAME=${CUTBEGINNING%.*} #${1}-${2}
-  ROOTFILENAME=${ROOTOUTDIR}${OUTPUTFILENAME}".root"
+  ROOTFILENAME=${OUTDIR}${OUTPUTFILENAME}".root"
 
   if [[ "$RESUBMIT" = true ]] && [[ ! -f ${ROOTFILENAME} ]]; then
     continue
   fi
 
+  if [[ "$file" == *"I0"* ]]; then
+    continue 
+  fi
+
   echo "Submitting "${file}
+  echo ${OUTPUTDIR}${OUTPUTFILENAME}".log"
   bsub -q psanaq -o${OUTPUTDIR}${OUTPUTFILENAME}".log" ./${FILETORUN} ${file}
+  #bsub -q psanaq -o${OUTPUTDIR}${OUTPUTFILENAME}".log" ./${FILETORUN} ${file} -computeCenters 1
+
+
+  #if [ COMPUTECENTERS ]; then
+
+  #  echo "HHHHHHHHEEEEEEEEEERRRRRRRRr"
+  #  bsub -q psanaq -o${OUTPUTDIR}${OUTPUTFILENAME}".log" ./${FILETORUN} ${file} -computeCenters 1
+  #else
+  #  bsub -q psanaq -o${OUTPUTDIR}${OUTPUTFILENAME}".log" ./${FILETORUN} ${file}
+  #fi
 
 done
